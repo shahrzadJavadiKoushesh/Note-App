979cc9159f76571e833eb2a0e22654f92ec9b1cb
shahrzad <shahrzadjkoushesh@gmail.com>
2023-06-01 10:58:18 +0330

    make noteslist dynamic but has errors

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,8 +1,42 @@
+import { useState } from "react";
+import {nanoid} from 'nanoid';
 import NotesList from "./components/NotesList";
 
 const App = () => {
+  const {notes, setNotes} = useState([
+    {
+    id: nanoid(),
+    text: "This is my first note.",
+    date: "1402/3/11",
+    },
+    {
+      id: nanoid(),
+      text: "This is my second note.",
+      date: "1402/3/11",
+    },
+    {
+      id: nanoid(),
+      text: "This is my third note.",
+      date: "1402/3/11",
+    },
+    {
+      id: nanoid(),
+      text: "This is my first note.",
+      date: "1402/3/11",
+    },
+    {
+      id: nanoid(),
+      text: "This is my first note.",
+      date: "1402/3/11",
+    },
+    {
+      id: nanoid(),
+      text: "This is my first note.",
+      date: "1402/3/11",
+    },
+]);
   return <div className="container">
-            <NotesList />
+            <NotesList notes={notes}/>
           </div>
 };
 
--- notes/src/components/Note.js
+++ notes/src/components/Note.js
@@ -1,10 +1,10 @@
 import {MdDeleteForever} from 'react-icons/md'
-const Note = () => {
+const Note = ({id, text, date}) => {
     return(
         <div className="note">
-            <span>Hello THis is my first note</span>
+            <span>{text}</span>
             <div className="note-footer">
-                <small>1400/3/7</small>
+                <small>{date}</small>
                 <MdDeleteForever className='delete-icon' size='1.3em' />
             </div>
         </div>
